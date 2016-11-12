03ab7f4e557a033b927148c6bb36cdee1bb7d505
reksarw <reksarw@gmail.com>
2016-11-12 18:40:48 +0700

    test

--- app/application/controllers/Kelola.php
+++ app/application/controllers/Kelola.php
@@ -1,16 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Kelola extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		
 	}
-
 	public function Barang_Masuk() {
 		$this->template->views('kelola/barang_masuk');
 	}
-
+	public function Pilih_Barang() {
+		$this->template->views('kelola/pilih_barang');
+	}
+	public function Add_Barang_Keluar() {
+		$this->template->views('kelola/add_barang_keluar');
+	}
 }
 
 /* End of file Kelola.php */
