9884bc12de66df2722cd8fecefd3835016c164b0
reksarw <reksarw@gmail.com>
2016-11-12 18:41:27 +0700

    tambah data kepabean

---
+++ app/application/views/tabel.data_kepabean.php
@@ -0,0 +1,55 @@
+<br>
+							<div class="text-center" style="font-weight:bold; font-size:20px;">
+                        		Data Kepabean
+                        	</div>
+
+                        	<div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Register Sita</label>
+	                                <input class="form-control" placeholder="No Register Sita" name="reg_sita">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Lama Sita</label>
+	                                <input class="form-control" placeholder="Lama Sita" name="lama">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Rak / Tempat</label>
+	                                <input class="form-control" placeholder="Lokasi Simpan" name="rak">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Kadaluarsa</label>
+	                                <input class="form-control" placeholder="Kadaluarsa" name="kadaluarsa">
+	                            </div>
+                            </div>
+							<div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Tanggal Masuk</label>
+	                                <input class="form-control" placeholder="Tanggal Masuk" name="tanggal_masuk">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nama Petugas 1</label>
+	                                <input class="form-control" placeholder="Nama Petugas 1" name="namapetugasa">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nama Petugas 2</label>
+	                                <input class="form-control" placeholder="Nama Petugas 2" name="namapetugasb">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Juru Sita</label>
+	                                <select name="juru_sita" class="form-control">
+	                                	<option value="1">Ini</option>
+	                                </select>
+	                            </div>
+                            </div>
