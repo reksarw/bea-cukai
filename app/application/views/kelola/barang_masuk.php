5eb5b36d74c1d0362fc25dcc19ad900acaa7273e
auwfar <achmad.chadilauwfar@gmail.com>
2016-11-13 19:59:19 +0700

    Update

--- app/application/views/kelola/barang_masuk.php
+++ app/application/views/kelola/barang_masuk.php
@@ -72,41 +72,173 @@
                         		Data Barang
                         	</div>
 
-                        	<div class="col-md-4">
+                        	<div class="col-md-3">
 	                            <div class="form-group">
-	                                <label>A. Nomor Surat</label>
-	                                <input class="form-control" placeholder="Nomor Surat" name="nomor_surat">
+	                                <label>Jenis Barang</label>
+	                                <select name="jenis_barang" class="form-control">
+	                                	<option value="1">Ini</option>
+	                                </select>
 	                            </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-3">
 	                            <div class="form-group">
-	                                <label>B. Lokasi Sita</label>
-	                                <input class="form-control" placeholder="Lokasi Sita" name="lokasi">
+	                                <label>Kondisi Barang</label>
+	                                <input class="form-control" placeholder="Kondisi Barang" name="kondisi_barang">
 	                            </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-3">
 	                            <div class="form-group">
-	                                <label>C. Tanggal Sita</label>
-	                                <input class="form-control" placeholder="Tanggal Sita" name="tgl_sita">
+	                                <label>Asal Barang</label>
+	                                <input class="form-control" placeholder="Asal Barang" name="asal">
 	                            </div>
                             </div>
 
-                            <div class="col-md-4">
+                            <div class="col-md-3">
 	                            <div class="form-group">
-	                                <label>A. No Disposisi</label>
-	                                <input class="form-control" placeholder="Nomor Disposisi" name="no_disposisi">
+	                                <label>Tujuan Barang</label>
+	                                <input class="form-control" placeholder="Tujuan Barang" name="tujuan">
 	                            </div>
                             </div>
-                            <div class="col-md-4">
+
+                            <div class="col-md-3">
 	                            <div class="form-group">
-	                                <label>B. Disposisi Awal</label>
-	                                <input class="form-control" placeholder="Disposisi Awal" name="disposisi_awal">
+	                                <label>No Dokumen</label>
+	                                <input class="form-control" placeholder="Nomor Dokumen" name="no_dok">
 	                            </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-3">
 	                            <div class="form-group">
-	                                <label>C. Disposisi Akhir</label>
-	                                <input class="form-control" placeholder="Disposisi Akhir" name="disposisi_akhir">
+	                                <label>Nomor Petikemas</label>
+	                                <input class="form-control" placeholder="Nomor Petikemas" name="no_peti">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Jumlah Barang</label>
+	                                <input class="form-control" placeholder="Jumlah Barang" name="jumlah">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Pengemasan</label>
+	                                <input class="form-control" placeholder="Pengemasan" name="pengemasan">
+	                            </div>
+                            </div>
+
+                            <br>
+							<div class="text-center" style="font-weight:bold; font-size:20px;">
+                        		Data Kepabeaan
+                        	</div>
+
+                        	<div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Invoice</label>
+	                                <input class="form-control" placeholder="Invoice" name="invoice">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>No Surat keputusan</label>
+	                                <input class="form-control" placeholder="Surat Keputusan" name="surat_keputusan">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nomor Register</label>
+	                                <input class="form-control" placeholder="No Register" name="no_register">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nomor Peti</label>
+	                                <input class="form-control" placeholder="No Peti" name="no_peti">
+	                            </div>
+                            </div>
+
+                            <br>
+							<div class="text-center" style="font-weight:bold; font-size:20px;">
+                        		Data Pemilik Barang
+                        	</div>
+
+                        	<div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nama Pemilik</label>
+	                                <input class="form-control" placeholder="Nama Pemilik" name="nama_pemilik">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Npwp</label>
+	                                <input class="form-control" placeholder="Npwp Pemilik" name="npwp">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Alamat Pemilik</label>
+	                                <input class="form-control" placeholder="Alamat Pemilik" name="alamat">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>TBP Kepabeaan</label>
+	                                <input class="form-control" placeholder="TBP Kepabeaan" name="tbp">
+	                            </div>
+                            </div>
+
+                            <br>
+							<div class="text-center" style="font-weight:bold; font-size:20px;">
+                        		Data Kepabean
+                        	</div>
+
+                        	<div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Register Sita</label>
+	                                <input class="form-control" placeholder="No Register Sita" name="reg_sita">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Lama Sita</label>
+	                                <input class="form-control" placeholder="Lama Sita" name="lama">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Rak / Tempat</label>
+	                                <input class="form-control" placeholder="Lokasi Simpan" name="rak">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Kadaluarsa</label>
+	                                <input class="form-control" placeholder="Kadaluarsa" name="kadaluarsa">
+	                            </div>
+                            </div>
+                            
+							<div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Tanggal Masuk</label>
+	                                <input class="form-control" placeholder="Tanggal Masuk" name="tanggal_masuk">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nama Petugas 1</label>
+	                                <input class="form-control" placeholder="Nama Petugas 1" name="namapetugasa">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Nama Petugas 2</label>
+	                                <input class="form-control" placeholder="Nama Petugas 2" name="namapetugasb">
+	                            </div>
+                            </div>
+                            <div class="col-md-3">
+	                            <div class="form-group">
+	                                <label>Juru Sita</label>
+	                                <select name="juru_sita" class="form-control">
+	                                	<option value="1">Ini</option>
+	                                </select>
 	                            </div>
                             </div>
 
