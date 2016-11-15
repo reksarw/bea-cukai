c792999c63091668a2c3653243c89eba9ef24fcd
reksarw <reksarw@gmail.com>
2016-11-16 02:03:39 +0700

    modal edit barang lelang

---
+++ app/application/views/modal/edit_barang_lelang.php
@@ -0,0 +1,54 @@
+<form role="form" method="POST" action="">
+    <div class="form-group">
+        <label>Kode Lelang</label>
+        <input type="text" class="form-control" placeholder="Kode Lelang" name="kode_lelang" value="KODE-LELANG-001" readonly>
+    </div>
+
+    <div class="form-group">
+        <label>Nomor Register Lelang</label>
+        <input type="text" class="form-control" placeholder="Register Lelang" name="reg_lelang">
+    </div>
+    <div class="form-group">
+        <label>Tujuan</label>
+        <input type="text" class="form-control" placeholder="Tujuan" name="tujuan">
+    </div>
+    <div class="form-group">
+        <label>Jumlah</label>
+        <input type="text" class="form-control" placeholder="Jumlah" name="jumlah">
+    </div>
+    <div class="form-group">
+        <label>Petugas A</label>
+        <input type="text" class="form-control" placeholder="Petugas A" name="petugasa">
+    </div>
+    <div class="form-group">
+        <label>Petugas B</label>
+        <input type="text" class="form-control" placeholder="Petugas B" name="petugasb">
+    </div>
+    <div class="form-group">
+        <label>Petugas C</label>
+        <input type="text" class="form-control" placeholder="Petugas C" name="petugasc">
+    </div>
+    <div class="form-group">
+        <label>Petugas D</label>
+        <input type="text" class="form-control" placeholder="Petugas D" name="petugasd">
+    </div>
+    <div class="form-group">
+        <label>Juru Sita 1</label>
+        <input type="text" class="form-control" placeholder="Juru Sita 1" name="jurusitaa">
+    </div>
+    <div class="form-group">
+        <label>Juru Sita 2</label>
+        <input type="text" class="form-control" placeholder="Juru Sita 2" name="jurusitab">
+    </div>
+    <div class="form-group">
+        <label>Kepala Juru Sita</label>
+        <input type="text" class="form-control" placeholder="Kepala Juru Sita" name="juru_sita">
+    </div>
+    <div class="form-group">
+        <label>Tanggal Lelang</label>
+        <input type="date" class="form-control" placeholder="Tanggal Lelang" name="tgl_lelang">
+    </div>
+    <button type="submit" class="btn btn-default">
+        <i class="fa fa-save"></i> Save
+    </button>
+</form>
